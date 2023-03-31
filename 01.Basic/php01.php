b668700e38f41b5696e08d54dc521b8da56a2622
Azajul-Karim <azajulkarim2021@gmail.com>
2023-03-31 16:08:39 +0600

    if else condition added

--- 01.Basic/php01.php
+++ 01.Basic/php01.php
@@ -84,3 +84,75 @@ $number = $number / 6; // 2
 $number = $number % 6; //0
 
 echo $number;
+echo "\n";
+
+$a = 7;
+$a++;
+echo $a;
+echo "\n";
+
+$b = 9;
+$b--;
+echo $b;
+echo "\n";
+
+
+
+//Number System
+$n = 12;
+$o = 012;
+$h = 0x2B;
+
+// printf("The Number Is %d and %d and %d , $n, $o, $h);
+
+printf("The Binary Equivalent Of %d is %b \n", 12, 12); //The Binary Equivalent Of 12 is 1100 
+
+printf("The Hexadecimal Equivalent Of %d is %x \n", 1212, 1212); //The Hexadecimal Equivalent Of 1212 is 4bc 
+
+printf("The Octal Equivalent Of %d is %o \n", 1212, 1212); //The Octal Equivalent Of 1212 is 2274 
+
+
+//PrintF
+$fName = "Azajul";
+$lName = "Karim";
+
+// printf("My Name Is %s %s \n", $fName, $lName);
+
+printf('My Name Is %2$s %1$s ', $fName, $lName); //My Name Is Karim Azajul 
+echo "\n";
+
+printf('The Binary Equivalent Of %1$d is %1$b', 12); //The Binary Equivalent Of 12 is 1100 
+
+echo "\n";
+
+$n = 45.2345;
+printf("%.2f", $n); //45.23
+echo "\n";
+
+$m = 123;
+$n = 27;
+printf("%04d \n", $m); //0123
+printf("%04d \n", $n); //0027
+
+$output = sprintf("My Name Is %s %s \n", $fName, $lName);
+
+echo strtoupper($output);
+
+
+//Logical Operator
+/*
+==
+!=
+>
+<
+>=
+<=
+*/
+
+$n = 13;
+
+if ($n % 2 == 0) {
+  echo "$n Is An Even Number";
+} else {
+  echo "$n Is An Odd Number";
+}
