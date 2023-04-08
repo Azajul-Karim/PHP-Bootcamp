a433bcfdc5ef31d3c952e88bb548dc286847ee36
Azajul-Karim <azajulkarim2021@gmail.com>
2023-04-08 15:49:51 +0600

    from added

--- 04.html_webpage/form.php
+++ 04.html_webpage/form.php
@@ -17,11 +17,21 @@
       <div class="column column-60 column-offset-20">
         <h2>Our First Form</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ab?</p>
+
+        <p>
+          <?php if (isset($_GET['fname']) && !empty($_GET['fname'])) { ?>
+            First Name: <?php echo $_GET['fname']; ?> <br>
+          <?php } ?>
+
+          <?php if (isset($_GET['lname']) && !empty($_GET['lname'])) { ?>
+            Last Name: <?php echo $_GET['lname']; ?> <br>
+          <?php } ?>
+        </p>
       </div>
     </div>
     <div class="row">
       <div class="column column-60 column-offset-20">
-        <form action="">
+        <form method="GET">
           <label for="fname">First Name</label>
           <input type="text" name="fname" id="fname">
 
