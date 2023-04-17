1e075517e7e1de8c4338781d43fa2493705fb2a8
Azajul-Karim <azajulkarim2021@gmail.com>
2023-04-17 14:47:32 +0600

    string

--- 06.string/string.php
+++ 06.string/string.php
@@ -14,8 +14,17 @@ echo $heredoc;
 
 
 //ASCII
-echo ord('A');
+// echo ord('A');
 echo "\n";
-echo ord('a');
+// echo ord('a');
 echo PHP_EOL;
-echo chr(98);
+// echo chr(98);
+
+
+$string = "Hello World";
+// echo $string[0];
+// echo $string[-5];
+$length = strlen($string);
+echo substr($string, $length - 3);
+echo PHP_EOL;
+echo substr($string, -7, 3);
