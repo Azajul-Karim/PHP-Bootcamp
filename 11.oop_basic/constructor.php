5de43fe5741d1965d1bc0f993288d57fcc35d550
Azajul-Karim <azajulkarim2021@gmail.com>
2023-05-16 13:16:46 +0600

    updated

--- 11.oop_basic/constructor.php
+++ 11.oop_basic/constructor.php
@@ -2,22 +2,33 @@
 class Human
 {
   public $name;
-
-  function __construct()
+  public $age;
+  function __construct($personName, $personAge = 0)
   {
-    echo "New Human";
+    // echo "New Human Object <br>";
+    $this->name = $personName;
+    $this->age = $personAge;
   }
+
   function sayHi()
   {
-    echo "Salam <br>";
+    echo "Hello <br>";
     $this->sayName();
   }
 
   function sayName()
   {
-    echo "My Name Is {$this->name}";
+    if ($this->age) {
+      echo "My Name Is {$this->name} , I Am {$this->age} Years Old .<br>";
+    } else {
+      echo "My Name Is {$this->name} , I Don't Know How Old I Am .<br>";
+    }
   }
 }
 
-$h1 = new Human();
+$h1 = new Human("Azajul", 23);
+$h2 = new Human("Topu", 24);
+$h3 = new Human("Rahi");
 $h1->sayHi();
+$h2->sayHi();
+$h3->sayHi();
